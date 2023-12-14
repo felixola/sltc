55d97eb67ab40654a07a12ce40b6b1d9188a0983
SteveGrey <greysteve1980@gmail.com>
2023-12-14 11:42:26 -0800

    prisma client

--- lib/db.ts
+++ lib/db.ts
@@ -4,8 +4,8 @@ declare global {
     var prisma: PrismaClient | undefined;
 };
 
-export const db = globalThis.prisma || new PrismaClient();
+// export const db = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV != "production") globalThis.prisma = db;
+// if (process.env.NODE_ENV != "production") globalThis.prisma = db;
 
-// export const db = new PrismaClient();
+export const db = new PrismaClient();
