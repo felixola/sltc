0ced1fce6b550e175aa6205f6ea732af19218727
SteveGrey <greysteve1980@gmail.com>
2023-12-14 09:30:48 -0800

    coursepage resolved

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,18 +1,18 @@
-// import { Button } from '@/components/ui/button';
-// import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
-// export const CoursePage = () => {
-//     return (
-//         <div className='p-6'>
+export const CoursePage = () => {
+    return (
+        <div className='p-6'>
 
-//             <Link href="/teacher/create">
+            <Link href="">
 
-//                 <Button>
-//                     New Course
-//                 </Button>
+                <Button>
+                    New Course
+                </Button>
 
-//             </Link>
+            </Link>
            
-//         </div>
-//     )
-// } 
+        </div>
+    )
+} 
