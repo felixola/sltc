b183553ef7f91e34b73aad3bfb319ba74fb6df75
SteveGrey <greysteve1980@gmail.com>
2023-12-14 09:34:10 -0800

    coursepage resolved

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,11 +1,13 @@
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import { NextPage } from 'next';
 
-export const CoursePage = () => {
+
+export const CoursePage: NextPage  = () => {
     return (
         <div className='p-6'>
 
-            <Link href="">
+            <Link href="/teacher/create">
 
                 <Button>
                     New Course
