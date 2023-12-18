cf3f5352517a2cae6df1d2723863519ec84ca27b
SteveGrey <greysteve1980@gmail.com>
2023-12-17 23:35:21 -0800

    sltc

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,11 +1,8 @@
 // export const isTeacher = (userId?: string | null) => {
 //   return userId === process.env.NEXT_PUBLIC_TEACHER_ID;
 // }
-
-
 export const isTeacher = (userId?: string | null) => {
   const authorizedTeacherIds = (process.env.NEXT_PUBLIC_TEACHER_ID || '').split(',').map(id => id.trim());
 
-  // Use strict equality to check if the userId is in the list of authorized teacher IDs
   return userId !== null && userId !== undefined && authorizedTeacherIds.includes(userId);
 };
